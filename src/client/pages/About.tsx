db2f35000d28043c80695bf546d617d27170cc50
Grigoriy Popov <grisha_mgn@bk.ru>
2024-07-21 15:28:35 +0500

    fix error

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -12,7 +12,7 @@ export const About: React.FC = () => {
         <div className="col">
           <h1>About</h1>
           <p>
-            Author: Hello World!
+            Author: Grigorii Popov
           </p>
         </div>
       </div>
